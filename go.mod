fa2432bb0eaa4ba6ece5388a06fdbeac03e2b1c4
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-08-19 07:50:41 +0530

    initial commit for making some basic routes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Riteshk1314/Ecommerce_Backend
+
+go 1.22.5
---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin",
+	"../controllers"
+
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp)
+	incomingRoutes.POST("/users/loging", controllers.Login)
+	incomingRoutes.POST("/admin/addproduct", controllers.AddProduct)
+	incomingRoutes.GET("/users/productview", controllers.ViewProduct)
+	incomingRoutes.GET("/users/search", controllers.Search)
+}
